7d6dd8857f6d93856c7b2ab2df0d1677a7817deb
Jack-chau <chaukaho149@gmail.com>
2026-08-28 01:14:47 +0800

    add route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './index.css';
 import Employees from './pages/Employees';
 import Header from './components/Header';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Customers from './pages/Customers'
+
 function App() {
   return (
     <Header>
-      <Employees/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/employees' element={<Employees/>}/>
+          <Route path='/customers' element={<Customers/>}/>
+        </Routes>
+      </BrowserRouter>
     </Header>
   );
 }
